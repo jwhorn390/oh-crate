4896fa9090a7755dbaf8f7a76faff01c85cffe6c
Jeffrey Horn <jwhorn@gmail.com>
2017-01-21 13:22:54 -0500

    Have not commited changes, added many things such as comments and other functions that create state changes

--- index.vr.js
+++ index.vr.js
@@ -23,21 +23,37 @@ class vrGame extends React.Component {
       enemies: [],
       crazy: false,
       timer: 0,
+      speedMultiplier: 1,
+      score: 0,
     };
+    // Used for timing length of game
+    this.timeOfGame = Date.now()
+    // Helpers for timing and rotation
     this.lastUpdate = Date.now();
     this.enterUpdate = null;
     this.rotateUpdate = Date.now();
+    // Crate movement
     this.move = this.move.bind(this);
     this.rotate = this.rotate.bind(this);
+    // Crate (enemy) creation
     this.createEnemies = this.createEnemies.bind(this);
     this.deleteEnemies = this.deleteEnemies.bind(this);
+    // Enemy destruction
     this.kill = this.kill.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
+    // Level changes
     this.increaseLevel = this.increaseLevel.bind(this);
     this.decreaseLevel = this.decreaseLevel.bind(this);
+    // Speed changes
+    this.increaseSpeed = this.increaseSpeed.bind(this);
+    this.decreaseSpeed = this.decreaseSpeed.bind(this);
   }
 
+// FOR START AND STOP BUTTONS 
+  
    createEnemies() {
+    this.deleteEnemies()
+    console.log('START')
     let numOfEnemies = 10* (this.state.levelMultiplier / 2)
     console.log(numOfEnemies)
     let enemiesArr = []
@@ -53,8 +69,13 @@ class vrGame extends React.Component {
   }
 
   deleteEnemies() {
+    console.log('STOP')
     this.setState({enemies: []})
+    this.setState({positionAdd: 0})
+    this.lastUpdate = Date.now()
   }
+
+// FOR CRATE MOVEMENT, MOVES THEM FORWARD, THE CHANGE DIVISOR CONTROLS THE RATE AT WHICH THEY MOVE
 
   move() {
     const now = Date.now();
@@ -65,11 +86,15 @@ class vrGame extends React.Component {
     this.frameHandleMove = requestAnimationFrame(this.move);
   }
 
+// SAME AS MOVE, EXCEPT FOR ROTATION
+
   rotate() {
     if (this.state.rotate === 180) this.setState({rotate: 0})
     this.setState({rotate: this.state.rotate + 20});
     this.frameHandleRotate = requestAnimationFrame(this.rotate)
   }
+
+// EXTRA CRAZY ROTATION, CONTINUALLY GETS LARGER WITHOUT RESETTING, CAUSING EXTRA LARGE ROTATION
 
   crayRotate() {
     const now = Date.now();
@@ -80,34 +105,63 @@ class vrGame extends React.Component {
     this.frameHandleRotate = requestAnimationFrame(this.crayRotate)
   }
 
+// FUNCTION FOR 'KILLING' CRATE
+
   kill(id) {
     console.log('IN KILL FUNC')
     const entered = Date.now()
     if (!this.enterUpdate) this.enterUpdate = Date.now();
     const change = entered - this.enterUpdate;
-    // console.log(document.getElementById(id))
-    this.frameHandleKill = requestAnimationFrame(this.kill(id))
-    // if (change > 200) React.unmountComponentAtNode(document.getElementById(id))
+    if (change > 500) {
+      var newArr = this.state.enemies.slice()
+      console.log(id)
+      newArr.splice(id,1)
+      this.setState({enemies: newArr})
+    }
   }
+
+// FUNCTION FOR RESETTING KILL TIMER (EXCEPT NOT USED AS OF NOW, LEFT IN FOR POSSIBLE USE LATER)
 
   resetTimer() {
     console.log('IN RESET TIMER')
     this.setState({timer: 0})
   }
 
+// CHANGES LEVEL (MULTIPLIER FOR # OF CRATES SPAWNED)
+
   increaseLevel() {
+    if (this.state.levelMultiplier === 10) return;
     this.setState({levelMultiplier: this.state.levelMultiplier+1})
   }
   
+// OPPOSITE OF increaseLevel()
+
   decreaseLevel() {
     if (this.state.levelMultiplier === 1) return;
     else this.setState({levelMultiplier: this.state.levelMultiplier-1})
   }
 
+// CHANGES SPEED (MULTIPLIER FOR RATE OF move())
+
+  increaseSpeed() {
+    if (this.state.speedMultiplier === 20) return;
+    this.setState({speedMultiplier: this.state.speedMultiplier+1})
+  }
+
+// OPPOSITE OF increaseSpeed()
+  
+  decreaseSpeed() {
+    if (this.state.speedMultiplier === 1) return;
+    else this.setState({speedMultiplier: this.state.speedMultiplier-1})
+  }
+
+// FOR WHEN COMPONENT MOUNTS (left blank for now)
 
   componentDidMount() {
  
   }
+
+// TO PREVENT ERRORS AFTER COMPONENT UNMOUNTS (does not unmount for now, entire app is one component with children inside of it)
 
   componentWillUnmount() {
     if (this.frameHandleMove) {
@@ -128,14 +182,16 @@ class vrGame extends React.Component {
   render() {
   
     return (
+      // APP VIEW
       <View
         style={{
           transform: [{translate: [0,0,-3]}],
           layoutOrigin: [0.5,0,0],
           alignItems: 'center',
         }}> 
+        {/*APP PANO aka static background, does not create actual 3D world (world with floor, ceiling, etc), but loads 3D image */}
         <Pano source={asset('chess-world.jpg')}/>
-        {
+        { //MAP FUNCTION TO DYNAMICALLY CREATE CRATES
           this.state.enemies.map((coords) =>  
             { 
               // console.log(coords)
@@ -149,24 +205,29 @@ class vrGame extends React.Component {
                 <Mesh  key={id} id={id} style={{
                   transform: [
                     {translate: [...coords]},
-                    {scale: 1},
+                    {scale: 2},
                     {rotateY: 180},
                     {rotateX: craziness},
-                    {translateZ: -this.state.positionAdd/20}
+                    {translateZ: -this.state.positionAdd/(50/this.state.speedMultiplier) }
                     ]
                   }}
                   source={{mesh:asset('Crate/Crate1.obj'), texture:asset('Crate/crate_1.jpg')}}
-                  // onMouseEnter={() => { this.kill(id) }}
-                  onGazeExit={() => { console.log('EXITTED GAZE') }}
+                  onMouseEnter={() => { this.kill(id) }}
+                  // onMouseExit unused for now
+                  onMouseExit={() => {this.resetTimer() }}
                 />
               
             )})
         }
         
    
-
+        {/*Add simple lighting*/}
         <PointLight style={{color:'white', transform:[{translate:[0,40,700]}]}} />
-        <View>
+        {/*View for all buttons, flexDirection is similar to "class = 'row'" in bootstrap*/}
+        <View style={{
+          flexDirection: 'row',
+          transform: [{translateY: -1}]
+        }}>
           <VrButton>
             <Text style={{
               fontSize: 1,
@@ -177,7 +238,7 @@ class vrGame extends React.Component {
               textAlign:'center',
               textAlignVertical:'center',
               transform: [
-                {translate: [-1.5,-3,0.2]},
+                {translate: [-1,0,0]},
                 {rotateX: -75}
               ]
             }}
@@ -197,7 +258,7 @@ class vrGame extends React.Component {
               textAlign:'center',
               textAlignVertical:'center',
               transform: [
-                {translate: [1.5,-2,0]},
+                {translate: [1,0,0]},
                 {rotateX: -75}
               ]
             }}
@@ -207,63 +268,127 @@ class vrGame extends React.Component {
             </Text>
           </VrButton>
         </View>
-        
-        <Text style={{
-          fontSize: 0.8,
-          paddingTop: 0.025,
-          paddingBottom: 0.025,
-          paddingLeft: 0.05,
-          paddingRight: 0.05,
-          textAlign:'center',
-          textAlignVertical:'center',
-          transform: [
-            {translate: [0,-3,-.1]},
-            {rotateX: -75}
-          ]
+       <View 
+        style = {{
+          flexDirection: 'row',
+          transform: [{translate: [0, -1, 0]}]
         }}>
-        LEVEL: {this.state.levelMultiplier}      
-        </Text>
-        
-        <VrButton>
           <Text style={{
-            fontSize: 1,
-            // paddingTop: 0.025,
-            // paddingBottom: 0.025,
-            // paddingLeft: 0.05,
-            // paddingRight: 0.15,
+            fontSize: 0.8,
+            paddingTop: 0.025,
+            paddingBottom: 0.025,
+            paddingLeft: 0.05,
+            paddingRight: 0.05,
             textAlign:'center',
             textAlignVertical:'center',
             transform: [
-              {translate: [-1,-3,0.2]},
+              {translate: [-1,-1,0]},
               {rotateX: -75}
             ]
-          }}
-          onEnter={()=> {this.increaseLevel()}}
-          >
-          +
+          }}>
+          LEVEL: {this.state.levelMultiplier}      
           </Text>
-        </VrButton>
-        
-        <VrButton>
           <Text style={{
-            fontSize: 1,
-            // paddingTop: 0.025,
-            // paddingBottom: 0.025,
-            // paddingLeft: 0.05,
-            // paddingRight: 0.15,
+            fontSize: 0.8,
+            paddingTop: 0.025,
+            paddingBottom: 0.025,
+            paddingLeft: 0.05,
+            paddingRight: 0.05,
             textAlign:'center',
             textAlignVertical:'center',
             transform: [
-              {translate: [1,-2.15,0]},
+              {translate: [1,-1,0]},
               {rotateX: -75}
             ]
-          }}
-          onEnter={()=> {this.decreaseLevel()}}
-          >
-          -
+          }}>
+          SPEED: {this.state.speedMultiplier}      
           </Text>
-        </VrButton>
+        </View>
+          <View style={{
+            flexDirection:'row',
+            transform: [{translate: [0,0,1]}]
+          }}>
+            <VrButton>
+              <Text style={{
+                fontSize: 1,
+                // paddingTop: 0.025,
+                // paddingBottom: 0.025,
+                // paddingLeft: 0.05,
+                // paddingRight: 0.15,
+                textAlign:'center',
+                textAlignVertical:'center',
+                transform: [
+                  {translate: [2,-1,0]},
+                  {rotateX: -75}
+                ]
+              }}
+              onEnter={()=> {this.decreaseSpeed()}}
+              >
+              -
+              </Text>
+            </VrButton>
 
+            <VrButton>
+              <Text style={{
+                fontSize: 1,
+                // paddingTop: 0.025,
+                // paddingBottom: 0.025,
+                // paddingLeft: 0.05,
+                // paddingRight: 0.15,
+                textAlign:'center',
+                textAlignVertical:'center',
+                transform: [
+                  {translate: [3,-1,0]},
+                  {rotateX: -75}
+                ]
+              }}
+              onEnter={()=> {this.increaseSpeed()}}
+              >
+              +
+              </Text>
+            </VrButton>
+
+            <VrButton>
+              <Text style={{
+                fontSize: 1,
+                // paddingTop: 0.025,
+                // paddingBottom: 0.025,
+                // paddingLeft: 0.05,
+                // paddingRight: 0.15,
+                textAlign:'center',
+                textAlignVertical:'center',
+                transform: [
+                  {translate: [-3,-1,0]},
+                  {rotateX: -75}
+                ]
+              }}
+              onEnter={()=> {this.decreaseLevel()}}
+              >
+              -
+              </Text>
+            </VrButton>
+
+            <VrButton>
+              <Text style={{
+                fontSize: 1,
+                // paddingTop: 0.025,
+                // paddingBottom: 0.025,
+                // paddingLeft: 0.05,
+                // paddingRight: 0.15,
+                textAlign:'center',
+                textAlignVertical:'center',
+                transform: [
+                  {translate: [-2,-1,0]},
+                  {rotateX: -75}
+                ]
+              }}
+              onEnter={()=> {this.increaseLevel()}}
+              >
+              +
+              </Text>
+            </VrButton>
+          </View>
+   
       </View>
     );
   }
